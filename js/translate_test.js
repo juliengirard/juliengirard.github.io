c4ca7aaf456f1e935785d4ce9909a9cb03c6c4c1
juliengirard <juliengirard@gmail.com>
2025-10-07 16:24:18 -0400

    minor changes

---
+++ js/translate_test.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script>
